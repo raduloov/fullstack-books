06044331f30f51d3aaea896f9d239e7c8c2fe301
raduloov <raduloov@gmail.com>
2022-06-05 14:10:27 +0300

    Clean up server code

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -120,27 +120,5 @@ module.exports = {
     const user = await User.findById(req.userId);
 
     return user.favoriteBooks;
-
-    // if (!page) {
-    //   page = 1;
-    // }
-    // const perPage = 2;
-    // const totalPosts = await Post.find().countDocuments();
-    // const posts = await Post.find()
-    //   .sort({ createdAt: -1 })
-    //   .skip((page - 1) * perPage)
-    //   .limit(perPage)
-    //   .populate('creator');
-    // return {
-    //   posts: posts.map(p => {
-    //     return {
-    //       ...p._doc,
-    //       _id: p._id.toString(),
-    //       createdAt: p.createdAt.toISOString(),
-    //       updatedAt: p.updatedAt.toISOString()
-    //     };
-    //   }),
-    //   totalPosts: totalPosts
-    // };
   }
 };
